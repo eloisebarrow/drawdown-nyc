6fa9b3b8c465eff277d6e5870e4c7d835ea7393c
eloisebarrow <eloisebarrow@gmail.com>
2020-02-16 16:52:12 -0500

    add image to About

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,20 @@ import React from 'react'
 export default function About() {
   return (
     <div className="about-container">
-      <div>
-        <h3>Who We Are</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
+      <div className="about-text">
+        <div>
+          <h3>Who We Are</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
+        </div>
+
+        <div>
+          <h3>What We Do</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
+        </div>
       </div>
 
-      <div>
-        <h3>What We Do</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
+      <div className="about-photo-container">
+        <img className="about-photo" alt="solar panels" src="https://res.cloudinary.com/eloise/image/upload/v1581881786/drawdown-nyc/solar-panels.jpg" />
       </div>
     </div>
   )
