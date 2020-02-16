aed0deeccb10af5c743733bf84862c3186e8f52d
eloisebarrow <eloisebarrow@gmail.com>
2020-02-16 18:10:27 -0500

    basic footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-container">
+      <div className="links-container">
+        <nav> Drawdown NYC
+          <a href="#">About</a>
+          <a href="#">Events</a>
+          <a href="#">Newsletter</a>
+          <a href="#">Contact</a>
+        </nav>
+
+        <nav> Project Drawdown
+          <a href="#">The Book</a>
+          <a href="#">Solutions</a>
+          <a href="#">Donate</a>
+          <a href="#">News and Media</a>
+          <a href="#">FAQs</a>
+        </nav>
+      </div>
+
+      <div className="copyright-container">
+        <p>Copyright 2020 Drawdown NYC. All rights reserved.</p>
+      </div>
     </div>
   )
 }
