311d3962218fdfe7e724226b2574b71cc5c4efa1
eloisebarrow <eloisebarrow@gmail.com>
2020-02-16 18:03:26 -0500

    corrected styles for About & Aside, added project drawdown section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="about-container">
       <div className="about-text">
-        <div>
+        <div className="about-who">
           <h3>Who We Are</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
         </div>
--- src/components/Aside.js
+++ src/components/Aside.js
@@ -9,15 +9,17 @@ export default function Aside() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
         </div>
 
-        <div>
+        <div className="newsletter-form-container">
           <h3>Stay In Touch</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet interdum elit. Vestibulum tempus rutrum pharetra. Suspendisse a eleifend nisl. Aenean nec rhoncus nunc, id eleifend nulla. Duis feugiat rhoncus dapibus. Fusce venenatis est eget ligula pellentesque, a porta diam gravida. Etiam tortor ex, tempus a enim vitae, molestie mattis purus.</p>
         </div>
       </div>
 
-      <div className="aside-drawdown-container">
-        
-      </div>
+      <a className="project-drawdown" href="https://www.drawdown.org/" target="_blank" rel="noopener noreferrer">
+        <div className="aside-drawdown-container">
+          <h4>Learn more about Project Drawdown</h4>
+        </div>
+      </a>
     </div>
   )
 }
