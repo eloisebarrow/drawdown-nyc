196b411868a8f0f2d3991e4892424b6dc51098ca
eloisebarrow <eloisebarrow@gmail.com>
2020-02-17 18:35:31 -0500

    wip lazy load attempts with react-lazy-load

--- src/components/IntroPhoto.js
+++ src/components/IntroPhoto.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+import LazyLoad from 'react-lazy-load';
 
 export default function IntroPhoto() {
   return (
     <div className="intro-photo-container">
-      <div className="intro-photo">
-        {/* background image in CSS */}
-        <h3>Bringing climate solutions to life, in NYC</h3>
-      </div>
+      <LazyLoad
+        width={100}
+        height={100}
+        debounce={false}
+        offsetVertical={500}>
+        <div>
+          {/* background image in CSS */}
+          <img className="intro-photo" alt="trees" src="https://res.cloudinary.com/eloise/image/upload/v1581953751/drawdown-nyc/alt-intro-photo02.jpg" />
+          <h3>Bringing climate solutions to life, in NYC</h3>
+        </div>
+      </LazyLoad>
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import IntroPhoto from './IntroPhoto';
+import React, { Suspense } from 'react';
 import About from './About';
 import Aside from './Aside';
+const IntroPhoto = React.lazy(() => import('./IntroPhoto'));
 
 export default function Main() {
+  
+  // const import('./IntroPhoto')
+  //   .then(({ IntroPhoto }) => {
+  //     return <IntroPhoto />
+  //   })
+  //   .catch(err => {
+  //     console.log(err, "IntroPhoto code split didn't work")
+  //   });
+
   return (
     <div>
-      <IntroPhoto />
+      <Suspense fallback={<div>Loading...</div>}>
+        <IntroPhoto />
+      </Suspense>
       <About />
       <Aside />
     </div>
